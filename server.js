9d23cf59d0670cf8e3010b66b24303fd27173c19
Bradley <bg9892@gmail.com>
2020-04-09 23:45:14 -0500

    added URI for heroku

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://bg9892:4Q#EDtckv3@GkTw@ds245518.mlab.com:45518/heroku_sh32xzcc", {
-//   useNewUrlParser: true
-// });
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget");
-
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://bg9892:4Q#EDtckv3@GkTw@ds245518.mlab.com:45518/heroku_sh32xzcc";
 mongoose.connect(MONGODB_URI);
 
@@ -26,5 +20,3 @@ app.use(require("./routes/html.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
-
