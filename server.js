9df8eb601b8ddf9cc8a38ad75be5096e0b05dea1
Bradley <bg9892@gmail.com>
2020-04-09 23:30:15 -0500

    add heroku credential

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://bg9892:4Q#EDtckv3@GkTw@ds245518.mlab.com:45518/heroku_sh32xzcc", {
-  useNewUrlParser: true
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://bg9892:4Q#EDtckv3@GkTw@ds245518.mlab.com:45518/heroku_sh32xzcc", {
+//   useNewUrlParser: true
+// });
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://bg9892:4Q#EDtckv3@GkTw@ds245518.mlab.com:45518/heroku_sh32xzcc";
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
